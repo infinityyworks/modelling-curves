7acbc4346acea83777c3dc4aeca633dee395ab84
Infinityworks <107305997+infinityyworks@users.noreply.github.com>
2026-05-11 09:35:42 +0100

    Created modelling-curves.py

---
+++ modelling-curves.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.stats import linregress
+from math import exp
+
+independentVar, dependentVar = input('Independent and dependent variable names: ').split(' ')
+independent = np.array(list(map(float, input(f'Dataset for {independentVar}: ').split(' '))))
+dependent = np.array(list(map(float, input(f'Dataset for {dependentVar}: ').split(' '))))
+
+ln_independent = np.log(independent)
+ln_dependent = np.log(dependent)
+
+
+# for exponential relationship in form y = k*b^x
+exponential = linregress(independent, ln_dependent)
+k = exp(exponential.intercept)
+b = exp(exponential.slope)
+
+print(f'\nFor a exponential relationship in the form {dependentVar} = k*b^{independentVar}:')
+print(f'Pearson correlation coefficient: {exponential.rvalue:.4f}')
+print(f'Equation: {dependentVar} = {k:.2f} * {b:.2f}^{independentVar}')
+
+
+# for power relationship in form y = a*x^n
+power = linregress(ln_independent, ln_dependent)
+a = exp(power.intercept)
+n = power.slope
+
+print(f'\nFor a power relationship in the form {dependentVar} = a*{independentVar}^n:')
+print(f'Pearson correlation coefficient: {power.rvalue:.4f}')
+print(f'Equation: {dependentVar} = {a:.2f} * {independentVar}^{n:.2f}')
